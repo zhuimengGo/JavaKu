c5ecef6c3c36d9ef3223d894ad477c6343095b47
zhuimengGo <zxx115@qq.com>
2016-07-09 16:05:38 +0800

    Revert "租房网ssh框架"
    This reverts commit 022aeb9dc91a1b44ae7ed31c5e27dc799f016ae0.

--- BirdHouse2/src/Dao/IUsersDao.java
+++
@@ -1,9 +0,0 @@
-package Dao;
-
-import pojo.Users;
-
-public interface IUsersDao {
-	void saveUser(Users u); 
-	Users findByName(String i);
-	void  saveorupdate(Users u); 
-}
--- BirdHouse2/src/Service/IUsersService.java
+++
@@ -1,9 +0,0 @@
-package Service;
-
-import pojo.Users;
-
-public interface IUsersService {
-	void saveUser(Users u); 
-	Users findByName(String i);
-	void  saveorupdate(Users u); 
-}
